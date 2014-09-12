d45dc85ea62b75ff4ca8d0483308fe3f56867146
Adam Crabtree <dude@noderiety.com>
2014-09-12 14:19:00 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node --harmony
+"use strict";
+
+let path = require('path')
+  , argv = require('yargs').argv._
+  , traceur = require('traceur')
+  , rc = require('rc')
+  , _ = require('lodash')
+  , tracuerConfig = _.omit(rc('traceur'), ['config', '_'])
+
+// Don't transpile dependencies
+function traceurFilter(filename) {
+	return filename.indexOf('node_modules') === -1
+}
+
+// Set traceur compiler with options as default require()
+traceur.require.makeDefault(traceurFilter, tracuerConfig)
+
+require(path.resolve(process.cwd(), argv[0]))
